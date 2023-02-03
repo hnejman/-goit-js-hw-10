59e29ecfe9d9305458b75df1c1bb423d77565586
hnejman <108231655+hnejman@users.noreply.github.com>
2023-02-03 15:20:45 +0100

    ..

--- parcel-project-template-main/src/index.js
+++
@@ -1,8 +0,0 @@
-import './css/styles.css';
-import notiflix from notiflix;
-
-const DEBOUNCE_DELAY = 300;
-
-fetch("https://restcountries.com/v3.1/name")
-.then(resolve=>{console.log(resolve)})
-.catch()
